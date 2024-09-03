0f92298cd9d079cf8df810316110fd2ec83d5000
Everythingisundercontrol <96534352+Everythingisundercontrol@users.noreply.github.com>
2024-09-03 15:05:06 +0800

    完成了胜利判断，增加了部分注释

--- Assets/Scripts/AddChess.cs
+++ Assets/Scripts/AddChess.cs
@@ -5,55 +5,46 @@ using UnityEngine;
 
 public class AddChess
 {
-    //名字
+    private int[,] _chessPositions;     //二维棋盘位置，索引指位置，值指无(-1)，黑(0)，白(1)
+    private readonly int _boardRows;             //棋盘总行列数
+    private readonly int _boardHalfRows;         //棋盘一个象限内行列数
+    private readonly float _boardLength;         //棋盘图片的长度
+    private readonly double _boardCellLength;    //棋盘格子边长
+    private int _stepCount;             //第几步，用以判断当前是下的白棋还是黑棋
+    private int _continuousCount;       //该点的某一轴上连续的棋子数
+    private bool _exitLoop;             //用以判断是否需要退出for循环
 
-    private int[,] _chessPositions;
-    private int _boardRows;
-    private int _boardHalfRows;
-    private float _boardLength;
-    private double _boardCellLength;
-    private int _stepCount;
-
-    private void Start()
+    public AddChess()
     {
+        _exitLoop = false;
         _boardLength = 5;
         _boardCellLength = 0.7;
         _boardRows = 15;
         _boardHalfRows = (_boardRows - 1) / 2;
-        _chessPositions = new int[_boardRows, _boardRows];
-        SetChessPositions();
-    }
-
-    private void Update()
-    {
-        InputCheck();
-    }
-
-    /// <summary>
-    /// 输入检测
-    /// </summary>
-    private void InputCheck()
-    {
-        if (!Input.GetMouseButtonDown(0))
-        {
-            return;
-        }
-
-        if (!Camera.main)
-        {
-            return;
-        }
-        
-        var clickScreenPointVector3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        var clickScreenPointVector2 = new Vector2(clickScreenPointVector3.x, clickScreenPointVector3.y);
-        AddChessPoint(clickScreenPointVector2);
+        _chessPositions = InitChessPositions(new int[_boardRows, _boardRows]);
     }
     
     /// <summary>
-    /// 
+    /// 初始化设置棋盘点位值为-1
+    /// </summary>
+    private static int[,] InitChessPositions(int[,] inputInts)
+    {
+        for (var i = 0; i < inputInts.Length; i++)
+        {
+            for (var j = 0; j < inputInts.Length; j++)
+            {
+                inputInts[i, j] = -1;
+            }
+        }
+            
+        return inputInts;
+    }
+    
+    /// <summary>
+    /// 添加棋子，然后检测是否胜利
     /// </summary>
     /// <param name="inputVector2"></param>
-    private void AddChessPoint(Vector2 inputVector2)
+    public void AddChessPoint(Vector2 inputVector2)
     {
         if (inputVector2.x > _boardLength || inputVector2.x < -_boardLength || inputVector2.y > _boardLength ||
             inputVector2.y < -_boardLength)
@@ -61,82 +52,212 @@ public class AddChess
             return;
         }
         var nearestChessPoint = NearestChessPoint(inputVector2);
-        if (CheckChessPointValue(nearestChessPoint)!=-1)
+        var x = (int) nearestChessPoint.x + _boardHalfRows;
+        var y = (int) nearestChessPoint.y + _boardHalfRows;
+        if (_chessPositions[x, y] !=-1)
         {
             return;
         }
 
-        _chessPositions[(int) nearestChessPoint.x, (int) nearestChessPoint.y] = _stepCount % 2;
+        _chessPositions[x, y] = _stepCount % 2;
         
         //加个添加棋子对象函数()
-        
-        WinCheck(nearestChessPoint);
+
+        WinCheck(x, y);
     }
 
     /// <summary>
-    /// 初始化设置棋盘点位值为-1
+    /// 胜利的动作
     /// </summary>
-    private void SetChessPositions()
+    private void Win()
     {
-        for (var i = 0; i < _chessPositions.Length; i++)
+        Debug.Log("win");
+    }
+
+    /// <summary>
+    /// 统计某一方向上连续的本色棋子数
+    /// </summary>
+    private void CalculateContinuousCount(int x, int y)
+    {
+        if (_chessPositions[x, y] == _stepCount % 2)
         {
-            for (var j = 0; j < _chessPositions.Length; j++)
-            {
-                _chessPositions[i, j] = -1;
-            }
+            _continuousCount++;
+        }
+
+        if (_chessPositions[x, y] != _stepCount % 2)
+        {
+            _exitLoop = true;
         }
     }
 
     /// <summary>
     /// 检测是否胜利
     /// </summary>
-    private void WinCheck(Vector2 inputVector2)
+    private void WinCheck(int x, int y)
     {
+        Horizontal(x, y);
+        if (_continuousCount >= 4)
+        {
+            Win();
+            return;
+        }
+        _continuousCount = 0;
         
+        Vertical(x, y);
+        if (_continuousCount >= 4)
+        {
+            Win();
+            return;
+        }
+        _continuousCount = 0;
+        
+        Diagonal(x, y);
+        if (_continuousCount >= 4)
+        {
+            Win();
+            return;
+        }
+        _continuousCount = 0;
+        
+        Antidiagonal(x, y);
+        if (_continuousCount >= 4)
+        {
+            Win();
+        }
+        _continuousCount = 0;
     }
 
     /// <summary>
     /// 水平方向
     /// </summary>
-    private void Horizontal()
+    private void Horizontal(int x, int y)
     {
+        //右方判断
+        for (var k = y + 1; k <= _boardRows - 1; k++)
+        {
+            CalculateContinuousCount(x, k);
+            if (!_exitLoop)
+            {
+                continue;
+            }
+            _exitLoop = true;
+            break;
+        }
         
+        //左方判断
+        for (var k = y - 1; k >= 0; k--)
+        {
+            CalculateContinuousCount(x, k);
+            if (!_exitLoop)
+            {
+                continue;
+            }
+            _exitLoop = true;
+            break;
+        }
+    }
+    
+    /// <summary>
+    /// 竖直方向
+    /// </summary>
+    private void Vertical(int x, int y)
+    {
+        //下方判断
+        for (var k = x + 1; k <= _boardRows - 1; k++)
+        {
+            CalculateContinuousCount(k, y);
+            if (!_exitLoop)
+            {
+                continue;
+            }
+            _exitLoop = true;
+            break;
+        }
+        
+        //上方判断
+        for (var k = x - 1; k >= 0; k--)
+        {
+            CalculateContinuousCount(k, y);
+            if (!_exitLoop)
+            {
+                continue;
+            }
+            _exitLoop = true;
+            break;
+        }
     }
 
     /// <summary>
     /// 对角线方向
     /// </summary>
-    private void Diagonal()
+    private void Diagonal(int x, int y)
     {
-        
+        //左上方判断
+        if (x != 0 && y != 0)
+        {
+            for (var k = 1; k <= x && k <= y; k++)
+            {
+                CalculateContinuousCount(x - k, y - k);
+                if (!_exitLoop)
+                {
+                    continue;
+                }
+                _exitLoop = true;
+                break;
+            }
+        }
+
+        //右下方判断
+        if (x == _boardRows - 1 || y == _boardRows - 1) 
+            return;
+        for (var k = 1; k <= _boardHalfRows - x; k++)
+        {
+            CalculateContinuousCount(x + k, y + k);
+            if (!_exitLoop)
+            {
+                continue;
+            }
+            _exitLoop = true;
+            break;
+        }
+
     }
 
     /// <summary>
     /// 反对角线方向
     /// </summary>
-    private void Antidiagonal()
+    private void Antidiagonal(int x, int y)
     {
-        
+        //右上方判断
+        if (x != 0 && y != _boardRows - 1)
+        {
+            for (var k = 1; k <= x && k <= _boardRows - 1 - y; k++)
+            {
+                CalculateContinuousCount(x - k, y + k);
+                if (!_exitLoop)
+                {
+                    continue;
+                }
+                _exitLoop = true;
+                break;
+            }
+        }
+
+        //左下方判断
+        if (x == _boardRows - 1 || y == 0) 
+            return;
+        for (var k = 1; k <= _boardRows - 1 - x && k <= y; k++)
+        {
+            CalculateContinuousCount(x + k, y + k);
+            if (!_exitLoop)
+            {
+                continue;
+            }
+            _exitLoop = true;
+            break;
+        }
     }
 
-    /// <summary>
-    /// 竖直方向
-    /// </summary>
-    private void Vertical()
-    {
-        
-    }
-    
-    /// <summary>
-    /// 检查棋盘点位的值
-    /// </summary>
-    /// <param name="inputVector2"></param>
-    /// <returns></returns>
-    private int CheckChessPointValue(Vector2 inputVector2)
-    {
-        return _chessPositions[(int) inputVector2.x,(int) inputVector2.y];
-    }
-    
     /// <summary>
     /// 返回最近的点
     /// </summary>
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Manager : MonoBehaviour
+    {
+        private AddChess addChess;
+
+        private void Start()
+        {
+            addChess = new AddChess();
+            
+        }
+
+        private void Update()
+        {
+            InputCheck();
+        }
+
+        
+        // ReSharper disable Unity.PerformanceAnalysis
+        /// <summary>
+        /// 输入检测
+        /// </summary>
+        private void InputCheck()
+        {
+            if (!Input.GetMouseButtonDown(0))
+            {
+                return;
+            }
+
+            if (!Camera.main)
+            {
+                return;
+            }
+        
+            var clickScreenPointVector3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var clickScreenPointVector2 = new Vector2(clickScreenPointVector3.x, clickScreenPointVector3.y);
+            addChess.AddChessPoint(clickScreenPointVector2);
+        }
+        
+
+    }
+}
