849eccf89e0fa016a4382da32e4b36d70f9f8c71
Everythingisundercontrol <96534352+Everythingisundercontrol@users.noreply.github.com>
2024-09-04 15:49:56 +0800

    可以玩的版本，但是可拓展性很差

--- Assets/Scripts/AddChess.cs
+++ Assets/Scripts/AddChess.cs
@@ -3,25 +3,43 @@ using UnityEngine;
 
 public class AddChess
 {
-    private int[,] _chessPositions;     //二维棋盘位置，索引指位置，值指无(-1)，黑(0)，白(1)
-    private readonly int _boardRows;             //棋盘总行列数
-    private readonly int _boardHalfRows;         //棋盘一个象限内行列数
-    private readonly float _boardLength;         //棋盘图片的长度
-    private readonly double _boardCellLength;    //棋盘格子边长
-    private int _stepCount;             //第几步，用以判断当前是下的白棋还是黑棋
-    private int _continuousCount;       //该点的某一轴上连续的棋子数
-    private bool _exitLoop;             //用以判断是否需要退出for循环
+    public int _stepCount; //第几步，用以判断当前是下的白棋还是黑棋
+    public readonly int _boardHalfRows; //棋盘一个象限内行列数
 
-    public AddChess()
+
+    private int[,] _chessPositions; //二维棋盘位置，索引指位置，值指无(-1)，黑(0)，白(1)
+    private readonly int _boardRows; //棋盘总行列数
+    private readonly float _boardLengthX; //棋盘图片x的长度
+    private readonly float _boardLengthY; //棋盘图片x的长度
+    private readonly double _boardCellLengthX; //棋盘格子X边长
+    private readonly double _boardCellLengthY; //棋盘格子X边长
+    private int _continuousCount; //该点的某一轴上连续的棋子数
+    private bool _exitLoop; //用以判断是否需要退出for循环
+
+    public AddChess(Vector2 inputVector, Vector2 inputVector1)
     {
         _exitLoop = false;
-        _boardLength = 5;
-        _boardCellLength = 0.7;
+        var x = Math.Abs(inputVector.x - inputVector1.x);
+        var y = Math.Abs(inputVector.y - inputVector1.y);
+        _boardLengthX = x;
+        _boardLengthY = y;
         _boardRows = 15;
         _boardHalfRows = (_boardRows - 1) / 2;
         _chessPositions = InitChessPositions(new int[_boardRows, _boardRows]);
+        _boardCellLengthX = Math.Round(_boardLengthX) / (_boardRows - 1);
+        _boardCellLengthY = Math.Round(_boardLengthY) / (_boardRows - 1);
     }
-    
+
+    // public AddChess()
+    // {
+    //     _exitLoop = false;
+    //     _boardLengthX = _boardLengthY = 5;
+    //     _boardCellLength = 0.7;
+    //     _boardRows = 15;
+    //     // _boardHalfRows = (_boardRows - 1) / 2;
+    //     _chessPositions = InitChessPositions(new int[_boardRows, _boardRows]);
+    // }
+
     /// <summary>
     /// 初始化设置棋盘点位值为-1
     /// </summary>
@@ -34,29 +52,34 @@ public class AddChess
                 inputInts[i, j] = -1;
             }
         }
-            
+
         return inputInts;
     }
-    
+
     /// <summary>
     /// 数据中添加棋子
     /// </summary>
     /// <param name="inputVector2"></param>
-    public Vector2? AddChessPoint(Vector2 inputVector2)
+    public Vector2 AddChessPoint(Vector2 inputVector2, Vector2 position1, Vector2 position2)
     {
-        if (inputVector2.x > _boardLength || inputVector2.x < -_boardLength || inputVector2.y > _boardLength ||
-            inputVector2.y < -_boardLength)
+        if (inputVector2.x < position1.x - 0.5 || inputVector2.x > position2.x + 0.5 ||
+            inputVector2.y < position1.y - 0.5 ||
+            inputVector2.y > position2.y + 0.5)
         {
-            return null;
+            return new Vector2(-1, -1);
         }
+
+        inputVector2.x += _boardHalfRows;
+        inputVector2.y += _boardHalfRows;
+
         var nearestChessPoint = NearestChessPoint(inputVector2);
-        var x = (int) nearestChessPoint.x + _boardHalfRows;
-        var y = (int) nearestChessPoint.y + _boardHalfRows;
-        if (_chessPositions[x, y] !=-1)
+        Debug.Log("near:" + nearestChessPoint);
+        if (_chessPositions[(int) nearestChessPoint.x, (int) nearestChessPoint.y] != -1)
         {
-            return null;
+            return new Vector2(-1, -1);
         }
-        _chessPositions[x, y] = _stepCount % 2;
+
+        _chessPositions[(int) nearestChessPoint.x, (int) nearestChessPoint.y] = _stepCount % 2;
         return nearestChessPoint;
     }
 
@@ -86,28 +109,34 @@ public class AddChess
         {
             return true;
         }
+
         _continuousCount = 0;
-        
+
         Vertical(x, y);
         if (_continuousCount >= 4)
         {
             return true;
         }
+
         _continuousCount = 0;
-        
+
         Diagonal(x, y);
+        // Debug.Log(_continuousCount);
         if (_continuousCount >= 4)
         {
             return true;
         }
+
         _continuousCount = 0;
-        
+
         Antidiagonal(x, y);
         if (_continuousCount >= 4)
         {
             return true;
         }
+
         _continuousCount = 0;
+        _stepCount++;
         return false;
     }
 
@@ -117,48 +146,19 @@ public class AddChess
     private void Horizontal(int x, int y)
     {
         //右方判断
-        for (var k = y + 1; k <= _boardRows - 1; k++)
-        {
-            CalculateContinuousCount(x, k);
-            if (!_exitLoop)
-            {
-                continue;
-            }
-            _exitLoop = true;
-            break;
-        }
-        
-        //左方判断
-        for (var k = y - 1; k >= 0; k--)
-        {
-            CalculateContinuousCount(x, k);
-            if (!_exitLoop)
-            {
-                continue;
-            }
-            _exitLoop = true;
-            break;
-        }
-    }
-    
-    /// <summary>
-    /// 竖直方向
-    /// </summary>
-    private void Vertical(int x, int y)
-    {
-        //下方判断
-        for (var k = x + 1; k <= _boardRows - 1; k++)
+        for (var k = x + 1; k < _boardRows; k++)
         {
             CalculateContinuousCount(k, y);
             if (!_exitLoop)
             {
                 continue;
             }
-            _exitLoop = true;
+
+            _exitLoop = false;
             break;
         }
-        
-        //上方判断
+
+        //左方判断
         for (var k = x - 1; k >= 0; k--)
         {
             CalculateContinuousCount(k, y);
@@ -166,7 +166,40 @@ public class AddChess
             {
                 continue;
             }
-            _exitLoop = true;
+
+            _exitLoop = false;
+            break;
+        }
+    }
+
+    /// <summary>
+    /// 竖直方向
+    /// </summary>
+    private void Vertical(int x, int y)
+    {
+        //上方判断
+        for (var k = y + 1; k < _boardRows; k++)
+        {
+            CalculateContinuousCount(x, k);
+            if (!_exitLoop)
+            {
+                continue;
+            }
+
+            _exitLoop = false;
+            break;
+        }
+
+        //下方判断
+        for (var k = y - 1; k >= 0; k--)
+        {
+            CalculateContinuousCount(x, k);
+            if (!_exitLoop)
+            {
+                continue;
+            }
+
+            _exitLoop = false;
             break;
         }
     }
@@ -177,42 +210,6 @@ public class AddChess
     private void Diagonal(int x, int y)
     {
         //左上方判断
-        if (x != 0 && y != 0)
-        {
-            for (var k = 1; k <= x && k <= y; k++)
-            {
-                CalculateContinuousCount(x - k, y - k);
-                if (!_exitLoop)
-                {
-                    continue;
-                }
-                _exitLoop = true;
-                break;
-            }
-        }
-
-        //右下方判断
-        if (x == _boardRows - 1 || y == _boardRows - 1) 
-            return;
-        for (var k = 1; k <= _boardHalfRows - x; k++)
-        {
-            CalculateContinuousCount(x + k, y + k);
-            if (!_exitLoop)
-            {
-                continue;
-            }
-            _exitLoop = true;
-            break;
-        }
-
-    }
-
-    /// <summary>
-    /// 反对角线方向
-    /// </summary>
-    private void Antidiagonal(int x, int y)
-    {
-        //右上方判断
         if (x != 0 && y != _boardRows - 1)
         {
             for (var k = 1; k <= x && k <= _boardRows - 1 - y; k++)
@@ -222,22 +219,61 @@ public class AddChess
                 {
                     continue;
                 }
-                _exitLoop = true;
+
+                _exitLoop = false;
+                break;
+            }
+        }
+
+        //右下方判断
+        if (x == _boardRows - 1 || y == 0)
+            return;
+        for (var k = 1; k <= _boardRows - 1 - x && k <= y; k++)
+        {
+            CalculateContinuousCount(x + k, y - k);
+            if (!_exitLoop)
+            {
+                continue;
+            }
+
+            _exitLoop = false;
+            break;
+        }
+    }
+
+    /// <summary>
+    /// 反对角线方向
+    /// </summary>
+    private void Antidiagonal(int x, int y)
+    {
+        //右上方判断
+        if (x != _boardRows - 1 && y != _boardRows - 1)
+        {
+            for (var k = 1; k <= _boardRows - 1 - x && k <= _boardRows - 1 - y; k++)
+            {
+                CalculateContinuousCount(x + k, y + k);
+                if (!_exitLoop)
+                {
+                    continue;
+                }
+
+                _exitLoop = false;
                 break;
             }
         }
 
         //左下方判断
-        if (x == _boardRows - 1 || y == 0) 
+        if (x == 0 || y == 0)
             return;
-        for (var k = 1; k <= _boardRows - 1 - x && k <= y; k++)
+        for (var k = 1; k <= x && k <= y; k++)
         {
-            CalculateContinuousCount(x + k, y + k);
+            CalculateContinuousCount(x - k, y - k);
             if (!_exitLoop)
             {
                 continue;
             }
-            _exitLoop = true;
+
+            _exitLoop = false;
             break;
         }
     }
@@ -249,7 +285,7 @@ public class AddChess
     /// <returns></returns>
     private Vector2 NearestChessPoint(Vector2 inputVector2)
     {
-        return new Vector2(FindClosestMultiple(inputVector2.x),FindClosestMultiple(inputVector2.y));
+        return new Vector2(FindClosestMultiple(inputVector2.x, 0), FindClosestMultiple(inputVector2.y, 1));
     }
 
     /// <summary>
@@ -257,19 +293,35 @@ public class AddChess
     /// </summary>
     /// <param name="x"></param>
     /// <returns></returns>
-    private int FindClosestMultiple(double x)
+    private int FindClosestMultiple(double x, int k)
     {
-        
-        var nFloor = Math.Floor(x / _boardCellLength);
-        var nCeil = Math.Ceiling(x / _boardCellLength);
-        
-        var distFloor = Math.Abs(_boardCellLength * nFloor - x);
-        var distCeil = Math.Abs(_boardCellLength * nCeil - x);
-        
+        double boardCellLength = 0;
+        switch (k)
+        {
+            case 0:
+            {
+                boardCellLength = _boardCellLengthX;
+                break;
+            }
+            case 1:
+            {
+                boardCellLength = _boardCellLengthY;
+                break;
+            }
+        }
+
+
+        var nFloor = Math.Floor(x / boardCellLength);
+        var nCeil = Math.Ceiling(x / boardCellLength);
+
+        var distFloor = Math.Abs(boardCellLength * nFloor - x);
+        var distCeil = Math.Abs(boardCellLength * nCeil - x);
+
         if (distFloor <= distCeil)
         {
-            return (int)nFloor;
+            return (int) nFloor;
         }
-        return (int)nCeil;
+
+        return (int) nCeil;
     }
 }
--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,12 +4,26 @@ namespace DefaultNamespace
 {
     public class Manager : MonoBehaviour
     {
+        public GameObject black;
+        public GameObject white;
+
+        public GameObject boardPositions1;
+        public GameObject boardPositions2;
+
+        public Vector2 vectorBoardPositions1;
+        public Vector2 vectorBoardPositions2;
+
         private AddChess _addChess;
+
 
         private void Start()
         {
-            _addChess = new AddChess();
-            
+            vectorBoardPositions1 =
+                new Vector2(boardPositions1.transform.position.x, boardPositions1.transform.position.y);
+            vectorBoardPositions2 =
+                new Vector2(boardPositions2.transform.position.x, boardPositions2.transform.position.y);
+            _addChess = new AddChess(vectorBoardPositions1, vectorBoardPositions2);
+            Debug.Log(vectorBoardPositions1 + ":::" + vectorBoardPositions2);
         }
 
         private void Update()
@@ -17,7 +31,7 @@ namespace DefaultNamespace
             InputCheck();
         }
 
-        
+
         // ReSharper disable Unity.PerformanceAnalysis
         /// <summary>
         /// 输入检测
@@ -33,18 +47,21 @@ namespace DefaultNamespace
             {
                 return;
             }
-        
+
             var clickScreenPointVector3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             var clickScreenPointVector2 = new Vector2(clickScreenPointVector3.x, clickScreenPointVector3.y);
-            var addChessPoint= _addChess.AddChessPoint(clickScreenPointVector2);
-            if (addChessPoint == null)
+            Debug.Log("AAA:" + clickScreenPointVector2);
+            var addChessPoint =
+                _addChess.AddChessPoint(clickScreenPointVector2, vectorBoardPositions1, vectorBoardPositions2);
+            if (addChessPoint.x < 0 || addChessPoint.y < 0)
             {
                 return;
             }
-            //加个添加棋子对象函数()
+
             addChessPrefab((Vector2) addChessPoint);
-            var WinOrLoss = _addChess.WinCheck((int) addChessPoint.Value.x, (int) addChessPoint.Value.y);
-            if (WinOrLoss)
+            // Debug.Log("BBB:"+addChessPoint);
+            var winOrLoss = _addChess.WinCheck((int) addChessPoint.x, (int) addChessPoint.y);
+            if (winOrLoss)
             {
                 Win();
             }
@@ -55,9 +72,23 @@ namespace DefaultNamespace
         /// </summary>
         private void addChessPrefab(Vector2 chess)
         {
-            Debug.Log(chess.x+":::"+chess.y);
+            Debug.Log("add:" + chess + " step:" + _addChess._stepCount);
+            if (_addChess._stepCount % 2 == 0)
+            {
+                Instantiate(black,
+                    new Vector3(chess.x - _addChess._boardHalfRows, chess.y - _addChess._boardHalfRows, 1),
+                    Quaternion.identity);
+            }
+
+            if (_addChess._stepCount % 2 == 1)
+            {
+                Instantiate(white,
+                    new Vector3(chess.x - _addChess._boardHalfRows, chess.y - _addChess._boardHalfRows, 1),
+                    Quaternion.identity);
+            }
         }
-    
+
+
         /// <summary>
         /// 胜利的动作
         /// </summary>
@@ -65,6 +96,5 @@ namespace DefaultNamespace
         {
             Debug.Log("win");
         }
-        
     }
 }
